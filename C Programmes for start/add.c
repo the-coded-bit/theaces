5ed9d1251de46da419dcdbb717842327f58e465c
Spandan Saxena <56041364+coder-chacha-reborn@users.noreply.github.com>
2019-10-01 23:02:12 +0530

    Corrected code alot
    Added and fixed certain parameters to fix the code

--- C Programmes for start/add.c
+++ C Programmes for start/add.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
-#include<conio.h>
 void main()
+{
 int a,b,c;
+printf("Enter the 1st Number\n");
+scanf("%d",&a);
+printf("Enter the 2nd Number\n"):
+scanf("%d",&b);
 c=a+b;
+printf("The sum is %d",c);
 }
